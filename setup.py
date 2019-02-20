883aa9e538ea7b13df2ba19e4a6a2ddbcfbe797a
Rodolfo N. Duldulao, Jr <rduldulao@salarium.com>
2019-02-20 09:49:33 +0800

    Added swagger_ui_headers_handlers and swagger_spec_headers describe kwargs, custom headers on swagger UI handlers and spec, minor code cleanup

--- setup.py
+++ setup.py
@@ -8,30 +8,30 @@ with open('README') as file:
 
 setup(name='tornado-swirl',
       python_requires='>=3.6.0',
-      version='0.1.14',
+      version='0.1.15',
       url='https://github.com/rduldulao/tornado-swirl',
       zip_safe=False,
       packages=['tornado_swirl'],
       package_data={
-        'tornado_swirl': [
-          'openapi/*.*',
-          'static/*.*',
-        ]
+          'tornado_swirl': [
+              'openapi/*.*',
+              'static/*.*',
+          ]
       },
       description='Extract swagger specs from your tornado project',
       author='Rodolfo Duldulao',
       license='MIT',
       long_description=long_description,
       install_requires=[
-        'tornado>=5.1.1'
+          'tornado>=5.1.1'
       ],
       classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.6'
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.6'
       ],
       keywords=['SWAGGER', 'OPENAPI', 'TORNADO'],
-      download_url='https://github.com/rduldulao/tornado-swirl/archive/v_0.1.14.tar.gz',
+      download_url='https://github.com/rduldulao/tornado-swirl/archive/v_0.1.15.tar.gz',
 )
